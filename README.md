8b32c154e6d02ca61b281e1b683dd999684218d9
Hervey Hall <1151070280>
2016-11-19 15:22:34 +0800

    This is a new era for herveyhall.cf

--- README.md
+++
@@ -1,4 +0,0 @@
-# HerveyHall.github.io
-个人博客
-#声明#
-本人的小站已转移至<a href="http://www.herveyhall.cf" target="_blank">www.herveyhall.cf</a>, 若访问本人的Github Page将自动跳转！
--- javascripts/bgImg.js
+++
@@ -1,5 +0,0 @@
-// 抓取必应每日图片
-var cbfunc = function(json){
-	var u = "https://www.bing.com"+json.query.results.images.image.url;
-	document.body.style.backgroundImage = "url("+u+")";
-	}

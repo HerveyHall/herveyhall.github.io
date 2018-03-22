9a9bd92a6d51f91518f1a105786882f7428b376e
HerveyHall <mail@herveyhall.cf>
2018-03-22 18:31:34 +0800

    通过canvas轮流显示多张bing图片
    Signed-off-by: HerveyHall <mail@herveyhall.cf>

--- public_html/javascripts/bgImg.js
+++ public_html/javascripts/bgImg.js
@@ -6,6 +6,10 @@
 
 // 抓取必应每日图片"https://www.bing.com/HPImageArchive.aspx?n=1&format=js"
 var cbfunc = function(json) {
-	_['bingImageUrl'] = "https://www.bing.com"
-			+ json.query.results.images.image.url;
+	var urlArr = json.query.results.images.image;
+	_['bingImageUrls'] = [];
+	for (var i = 0; i < urlArr.length; ++i) {
+		_['bingImageUrls'][i] = "https://www.bing.com"
+				+ json.query.results.images.image[i].url;
+	}
 }
--- public_html/javascripts/main.js
+++ public_html/javascripts/main.js
@@ -15,7 +15,7 @@ var _ = {};
 			document.getElementById("sub_title").innerHTML = "";
 		}
 	}
-	_['showInserter'] = function showInserter() {
+	_['showInserter'] = function() {
 		document.getElementById("cursor").innerHTML = "█";
 		setTimeout("document.getElementById('cursor').innerHTML='_';", 500);
 	}
@@ -25,7 +25,47 @@ this.onload = function() {
 	// this.showTitle();
 	this.setInterval("_.showSubTitle();", 300);
 	this.setInterval("_.showInserter();", 1000);
-	document.getElementById("bg").src = _.bingImageUrl;
+	var canvasBg = document.createElement("canvas");
+	if (canvasBg.getContext) {
+		_['canvasProperties'] = {};
+		canvasBg.width = '1000';
+		canvasBg.height = '1000';
+		canvasBg.id = "bg-canvas"
+		document.getElementsByTagName("body").item(0).appendChild(canvasBg);
+		_['canvasProperties']['context'] = canvasBg.getContext("2d");
+		_['canvasProperties']['bgImgs'] = [];
+		_['imgLoaderProperties'] = {};
+		_['imgLoaderProperties']['loadedImgCount'] = 0;
+		for (var i = 0; i < _.bingImageUrls.length; ++i) {
+			_.canvasProperties.bgImgs[i] = new Image();
+			_.canvasProperties.bgImgs[i].src = _.bingImageUrls[i];
+			_.canvasProperties.bgImgs[i].onload = function() {
+				++_['imgLoaderProperties']['loadedImgCount'];
+				console.log(_['imgLoaderProperties']['loadedImgCount']);
+			}
+		}
+		_['canvasProperties']['currentImg'] = 0;
+		_['canvasBgScroll'] = function() {
+			_.canvasProperties.context.clearRect(0, 0, canvasBg.width,
+					canvasBg.height);
+			_.canvasProperties.currentImg = _.canvasProperties.currentImg < _.canvasProperties.bgImgs.length ? _.canvasProperties.currentImg
+					: 0;
+			console.log(_.canvasProperties.currentImg,
+					_.canvasProperties.bgImgs[_.canvasProperties.currentImg]);
+			_.canvasProperties.context.drawImage(
+					_.canvasProperties.bgImgs[_.canvasProperties.currentImg++],
+					0, 0, canvasBg.width, canvasBg.height);
+		}
+		_['imgLoaderProperties']['time'] = 0;
+		_['imgLoaderProperties']['maxTime'] = 60000;
+		_['imgLoaderProperties']['intervalLength'] = 100;
+		_['imgLoaderProperties']['intervalID'] = this
+				.setInterval(
+						"(function() {if (_.imgLoaderProperties.loadedImgCount == _['canvasProperties']['bgImgs'].length && _.imgLoaderProperties.time < _.imgLoaderProperties.maxTime) {_.canvasBgScroll(); this.setInterval('_.canvasBgScroll();', 2000); clearInterval(_.imgLoaderProperties.intervalID); _.imgLoaderProperties = undefined;} else {_['imgLoaderProperties']['time'] += _.imgLoaderProperties.intervalLength;}})();",
+						_.imgLoaderProperties.intervalLength);
+	} else {
+		document.getElementById("bg").src = _.bingImageUrls[0];
+	}
 	// 页面加载完成，设置div#content为透明
 	document.getElementById("content").style.opacity = 0.3;
 }

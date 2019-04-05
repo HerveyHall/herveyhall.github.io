09ce7f950f9f4df8661a12b1a5f684d98fc7ef4b
HerveyHall <mail@herveyhall.cf>
2019-04-06 01:01:27 +0800

    将YahooAPI换为JsonBird，同时修改返回数据的接收格式
    Signed-off-by: HerveyHall <mail@herveyhall.cf>

--- public_html/javascripts/bgImg.js
+++ public_html/javascripts/bgImg.js
@@ -6,10 +6,10 @@
 
 // 抓取必应每日图片"https://www.bing.com/HPImageArchive.aspx?n=1&format=js"
 var cbfunc = function(json) {
-	var urlArr = json.query.results.images.image;
+	var urlArr = json.images;
 	_['bingImageUrls'] = [];
 	for (var i = 0; i < urlArr.length; ++i) {
 		_['bingImageUrls'][i] = "https://www.bing.com"
-				+ json.query.results.images.image[i].url;
+				+ json.images[i].url;
 	}
 }
